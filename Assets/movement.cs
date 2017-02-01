97081c6011bf5d0ea3589877e1f5dd23450232e0
Unknown <allisongameyfuntimes@gmail.com>
2017-02-01 16:17:46 -0500

    First change! initial project unity. fencers move and lunge. no damage detection or time slow. score set, doesn't change

---
+++ Assets/movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    float step = .5f;
+    public Sprite lunge;
+    public Sprite engarde;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	if(Input.GetKeyDown("d"))
+        {
+            //Vector3 currentPos = transform.position;
+            //currentPos.x += 5;
+            //transform.position = currentPos;
+
+            transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+
+        }
+    if(Input.GetKeyDown("a"))
+        {
+            transform.position = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            GetComponent<SpriteRenderer>().sprite = lunge;
+        }
+        else if(Input.GetKeyUp("s"))
+        {
+            GetComponent<SpriteRenderer>().sprite = engarde;
+        }
+    }
+}
---
+++ Assets/movement2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement2 : MonoBehaviour {
+
+    float step = 0.5f;
+    public Sprite lunge;
+    public Sprite engarde;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("j"))
+        {
+            transform.position = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown("l"))
+        {
+            transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown("k"))
+        {
+            GetComponent<SpriteRenderer>().sprite = lunge;
+        }
+        else if (Input.GetKeyUp("k"))
+        {
+            GetComponent<SpriteRenderer>().sprite = engarde;
+        }
+    }
+}
