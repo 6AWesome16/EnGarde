03818f8c12730847363d75f78c9d07973c9c2cf9
Unknown <allisongameyfuntimes@gmail.com>
2017-02-08 16:51:05 -0500

    movement code now contains both players. a lot more buggy. players reset on one side but they are hugging/

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 public class movement : MonoBehaviour {
 
     float step = .5f;
-    public movement2 otherplayer;
+    public movement otherplayer;
     public bool attacking = false;
     public int score = 0;
     public Text scoretext;
     bool touch = false;
-    
+    public int playerNumber;
+    public Vector3 resetPos;
 
     // Use this for initialization
     void Start () {
@@ -20,37 +21,68 @@ public class movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         scoretext.text = "points:" + score;
+        if (playerNumber == 1)
+        {
+            if (Input.GetKeyDown("d"))
+            {
+                //Vector3 currentPos = transform.position;
+                //currentPos.x += 5;
+                //transform.position = currentPos;
 
-        if (Input.GetKeyDown("d"))
-        {
-            //Vector3 currentPos = transform.position;
-            //currentPos.x += 5;
-            //transform.position = currentPos;
-
-            transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
-
-        }
-    if(Input.GetKeyDown("a"))
-        {
-            transform.position = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
-        }
-        if (Input.GetKeyDown("s"))
-        {
-         //   GetComponent<SpriteRenderer>().sprite = lunge;
-            GetComponent<Animator>().SetBool("attacking", true);
-            attacking = true;
-        }
-        else if(Input.GetKeyUp("s"))
-        {
-           // GetComponent<SpriteRenderer>().sprite = engarde;
-            GetComponent<Animator>().SetBool("attacking", false);
-            attacking = false;
-        }
-        if(attacking == false && score > 4)
+                transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                transform.position = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
+            }
+            if (Input.GetKeyDown("s"))
+            {
+                //   GetComponent<SpriteRenderer>().sprite = lunge;
+                GetComponent<Animator>().SetBool("attacking", true);
+                attacking = true;
+            }
+            else if (Input.GetKeyUp("s"))
+            {
+                // GetComponent<SpriteRenderer>().sprite = engarde;
+                GetComponent<Animator>().SetBool("attacking", false);
+                attacking = false;
+            }
+            if (attacking == false && score > 4)
             {
                 score = 0;
                 otherplayer.score = 0;
             }
+        }
+        else if(playerNumber == 2)
+        {
+
+            if (Input.GetKeyDown("j"))
+            {
+                transform.position = new Vector3(transform.position.x - step, transform.position.y, transform.position.z);
+            }
+            if (Input.GetKeyDown("l"))
+            {
+                transform.position = new Vector3(transform.position.x + step, transform.position.y, transform.position.z);
+            }
+
+            if (Input.GetKeyDown("k"))
+            {
+                //GetComponent<SpriteRenderer>().sprite = lunge;
+                GetComponent<Animator>().SetBool("attacking", true);
+                attacking = true;
+            }
+            else if (Input.GetKeyUp("k"))
+            {
+                //GetComponent<SpriteRenderer>().sprite = engarde;
+                GetComponent<Animator>().SetBool("attacking", false);
+                attacking = false;
+            }
+            if (attacking == false && score > 4)
+            {
+                score = 0;
+                otherplayer.score = 0;
+            }
+        }
     }
     void OnTriggerEnter2D(Collider2D collisioninfo)
     {
@@ -64,8 +96,8 @@ public class movement : MonoBehaviour {
                 touch = true;
                 if (touch)
                 {
-                    transform.position = new Vector3(-4.6f, -2.65f);
-                    otherplayer.transform.position = new Vector3(4.6f, -2.65f);
+                    transform.position = resetPos;
+                    otherplayer.transform.position = resetPos;
                 }
             }
         }
